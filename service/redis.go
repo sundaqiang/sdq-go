b97cf31778ec545f5204225f471c16b043b00ad3
Johnny <393116105@qq.com>
2025-06-24 20:11:01 +0800

    test: ✅ 测试SetNX

--- service/redis.go
+++ service/redis.go
@@ -135,7 +135,7 @@ func (r *Redis) initRedis() {
 		WriteTimeout: 3 * time.Second, // 写超时，默认等于读超时
 		PoolTimeout:  4 * time.Second, // 代表如果连接池所有连接都在使用中，等待获取连接时间，超时将返回错误 默认是 1秒+ReadTimeout
 		// 命令执行失败时的重试策略
-		MaxRetries:      3,                      // 命令执行失败时，最多重试多少次，默认为0即不重试
+		MaxRetries:      0,                      // 命令执行失败时，最多重试多少次，默认为0即不重试
 		MinRetryBackoff: 8 * time.Millisecond,   // 每次计算重试间隔时间的下限，默认8毫秒，-1表示取消间隔
 		MaxRetryBackoff: 512 * time.Millisecond, // 每次计算重试间隔时间的上限，默认512毫秒，-1表示取消间隔
 	})
